7217b1d7af36e2b317a6c37d182eb59df09d7381
berwyn <berwyn.codeweaver@gmail.com>
2017-07-23 22:41:34 -0600

    feat(site): Better cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import { style } from 'typestyle'
+import Chip from 'material-ui/Chip'
+import Avatar from 'material-ui/Avatar'
 import { GridList, GridTile } from 'material-ui/GridList'
 import { Card, CardMedia } from 'material-ui/Card'
 import * as logo from '../images/stormblood.png'
@@ -13,62 +15,163 @@ const rootClass = style({
 
 const logoClass = style({
   maxWidth: '80vw',
-  maxHeight: '70vh',
-  alignSelf: 'center'
+  maxHeight: '50vh',
+  alignSelf: 'center',
+  marginBottom: 32,
 })
 
 const titleClass = style({
   margin: 0
 })
 
-const images = [
+const stripClass = style({
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'nowrap',
+  overflowX: 'auto',
+  paddingLeft: 8,
+  paddingBottom: 8,
+})
+
+interface Tag {
+  name: string
+  avatar: any
+}
+
+interface Image {
+  key: string | number
+  image: any,
+  tags: Tag[]
+}
+
+const images: Image[] = [
   {
     key: 1,
-    image: logo
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
   },
   {
     key: 2,
-    image: logo
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+      { avatar: logo, name: 'Coconi Coni' },
+    ],
   },
   {
     key: 3,
-    image: logo
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+      { avatar: logo, name: 'Arcane Gem' },
+      { avatar: logo, name: 'Coconi Coni' },
+      { avatar: logo, name: 'Walmar Temployee' },
+    ],
   },
   {
     key: 4,
-    image: logo
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+      { avatar: logo, name: `X'rhashi Karimak` },
+    ],
   },
   {
     key: 5,
-    image: logo
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
   },
   {
     key: 6,
-    image: logo
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
+  },
+  {
+    key: 7,
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
+  },
+  {
+    key: 8,
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
+  },
+  {
+    key: 9,
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
+  },
+  {
+    key: 10,
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
+  },
+  {
+    key: 11,
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
+  },
+  {
+    key: 12,
+    image: logo,
+    tags: [
+      { avatar: logo, name: 'Ruby Onrailles' },
+    ],
   },
 ]
 
 export default class IndexPage extends React.Component<{}, {}> {
   render() {
-    console.log(logo)
-    console.log('test?')
     return (
       <div className={rootClass}>
         <img className={logoClass} src={logo} />
         <GridList
-          cellHeight={180}>
-          {images.map(i => {
-            return (
-              <GridTile key={i.key}>
-                <Card>
-                  <CardMedia>
-                    <img src={i.image} />
-                  </CardMedia>
-                </Card>
-              </GridTile>
-            )
-          })}
+          cols={3}
+          cellHeight={'auto'}>
+          {images.map(i => this.buildCard(i))}
         </GridList>
+      </div>
+    )
+  }
+
+  private buildCard(image: Image): JSX.Element {
+    return (
+      <GridTile key={image.key}>
+        <Card>
+          <CardMedia
+            overlay={this.buildTags(image.tags)}>
+            <img src={image.image} />
+          </CardMedia>
+        </Card>
+      </GridTile>
+    )
+  }
+
+  private buildTags(tags: Tag[]): JSX.Element {
+    return (
+      <div className={stripClass}>
+        {tags.map((t, i) => (
+          <Chip key={i} style={{ marginRight: 8 }}>
+            <Avatar src={t.avatar} />
+            {t.name}
+          </Chip>
+        ))}
       </div>
     )
   }
